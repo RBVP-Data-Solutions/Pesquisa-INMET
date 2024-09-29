64a302317fa8a79ba9c683ce051fe2b0f4475d7b
Vanessa <vanessa.gonsil99@gmail.com>
2024-09-29 19:07:44 -0300

    Alteração no airflow

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #AIRFLOW
 sudo rm -rf airflow;
 sudo mkdir airflow;
-sudo chmod 777 airflow;
+sudo chmod 755 airflow;
 cd airflow;
 sudo curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.5.3/docker-compose.yaml';
-sudo chmod 777 docker-compose.yaml;
+sudo chmod 644 docker-compose.yaml;
 sudo mkdir -p ./dags ./logs ./plugins ./scripts ./datasets;
 sudo echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env;
-sudo chmod 777 dags;
-sudo chmod 777 logs;
-sudo chmod 777 plugins;
-sudo chmod 777 scripts;
-sudo chmod 777 datasets;
+sudo chmod 755 dags;
+sudo chmod 755 logs;
+sudo chmod 755 plugins;
+sudo chmod 755 scripts;
+sudo chmod 755 datasets;
 cd ..
 
 #KAFKA
